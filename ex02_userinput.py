19ee85a4e8e0508b9fc0db22bbe5c9eaf25bf9b8
Gaurab Kumar <progaurab@gmail.com>
2023-12-01 17:35:30 +0530

    added example

---
+++ ex02_userinput.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum = num1 + num2
+
+print("Sum: ", sum)
---
+++ ex03_import_lib.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * (radius ** 2)
+
+print("Area of the circle: ", area)
