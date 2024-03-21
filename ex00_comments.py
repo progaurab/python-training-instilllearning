d2e03de6057bc0267d2a1025cbb033478213223d
Gaurab Kumar <progaurab@gmail.com>
2024-03-21 02:49:17 +0000

    added ex00

---
+++ ex00_comments.py
@@ -0,0 +1,13 @@
+# this single line comments in python
+
+'''
+    this is
+    multiline
+    comments
+''' 
+
+"""
+    this is also multi line comments
+    and
+    this will not be printed!!
+"""
